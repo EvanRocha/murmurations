85b151dbcb7da9cb97ec02c376cc52e2b0acfeeb
Evan <the.evanrocha@gmail.com>
2020-05-09 20:28:20 -0700

    some more naming and clean up

--- js/Boid3DRenderer.js
+++ js/Boid3DRenderer.js
@@ -4,13 +4,13 @@ import {
 } from './OrbitControls.js';
 
 const NUM_PARTICLES = 1000;
-const PARTICLE_SCALE = 0.6;
-const INITIAL_POSITION_RANGE = 10;
+const PARTICLE_SCALE = 0.65;
+const INITIAL_POSITION_RANGE = 1;
 
 // Movement
-const INITIAL_PARTICLE_VELOCITY = 0.20;
+const INITIAL_PARTICLE_VELOCITY = 0.405;
 const MAX_PARTICLE_VELOCITY = 0.405;
-const PARTICLE_TURN_ACCELERATION = 0.04;
+const PARTICLE_TURN_ACCELERATION = 0.039;
 
 // Social
 const PARTICLE_VISUAL_RANGE = 2;
@@ -27,7 +27,232 @@ const Z_BOUNDARY = 10;
 const CAMERA_FIELD_OF_VIEW = 75;
 const CAMERA_MAX_POSITION = 75;
 
-// Random Helpers:
+let scene = null;
+let renderer = null;
+let camera = null;
+let cameraControls = null;
+let cameraBoundarySphere = null;
+let particleSystemGeometry = null;
+
+function init() {
+	initRenderer();
+	initScene();
+	initCamera();
+	initSkyBox();
+	initParticles();
+
+	window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
+}
+
+function animate() {
+
+	particleSystemGeometry.vertices.map(particle => {
+		// Update particle velocity
+		updateVelocityForSocialAttraction(particle);
+		updateVelocityForSocialDistancing(particle);
+		updateVelocityForSocialCohesion(particle);
+		updateVelocityForVelocityLimit(particle);
+		updateVelocityForPositionBoundary(particle);
+
+		// Update particle position from velocity
+		particle.add(particle.velocity);
+	});
+
+	particleSystemGeometry.verticesNeedUpdate = true;
+
+	// update camera
+	cameraControls.update();
+	updatePositionForBoundary(camera);
+
+	// render
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+};
+
+function initRenderer() {
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+}
+
+function initScene() {
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x0f0f0f);
+}
+
+function initCamera() {
+	camera = new THREE.PerspectiveCamera(
+		CAMERA_FIELD_OF_VIEW,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000,
+	);
+	camera.position.z = 35;
+
+	// Sphere to bound the particles
+	cameraBoundarySphere = new THREE.Sphere(new THREE.Vector3(), CAMERA_MAX_POSITION);
+
+	cameraControls = new OrbitControls(camera, renderer.domElement);
+	cameraControls.update();
+}
+
+function initSkyBox() {
+	let cols = [{
+		stop: 0,
+		color: new THREE.Color(0xf7b000)
+	}, {
+		stop: .35,
+		color: new THREE.Color(0xdd0080)
+	}, {
+		stop: .5,
+		color: new THREE.Color(0x622b85)
+	}, {
+		stop: .75,
+		color: new THREE.Color(0x007dae)
+	}, {
+		stop: 1,
+		color: new THREE.Color(0x77c8db)
+	}];
+
+	let geometry = new THREE.SphereGeometry(100, 100, 100);
+	let material = new THREE.MeshBasicMaterial({
+		vertexColors: THREE.VertexColors,
+		wireframe: false
+	});
+	setGradient(geometry, cols, 'y', false);
+	material.side = THREE.BackSide;
+	scene.add(new THREE.Mesh(geometry, material));
+
+}
+
+function initParticles() {
+	particleSystemGeometry = new THREE.Geometry();
+	let particleMaterial = new THREE.PointsMaterial({
+		color: 0x000000,
+		size: PARTICLE_SCALE,
+		map: new THREE.TextureLoader().load('../textures/sprites/softcircle.png'),
+		sizeAttenuation: true,
+		alphaTest: 0.5,
+		transparent: true,
+	});
+
+	// now create the individual particles
+	for (var p = 0; p < NUM_PARTICLES; p++) {
+		const particle = new THREE.Vector3(
+			Math.random() * 2 * INITIAL_POSITION_RANGE - INITIAL_POSITION_RANGE,
+			Math.random() * 2 * INITIAL_POSITION_RANGE - INITIAL_POSITION_RANGE,
+			Math.random() * 2 * INITIAL_POSITION_RANGE - INITIAL_POSITION_RANGE,
+		);
+
+		// create a velocity vector. Hackily add it to the particle object
+		particle.velocity = new THREE.Vector3(
+			Math.random() * 2 * INITIAL_PARTICLE_VELOCITY - INITIAL_PARTICLE_VELOCITY,
+			Math.random() * 2 * INITIAL_PARTICLE_VELOCITY - INITIAL_PARTICLE_VELOCITY,
+			Math.random() * 2 * INITIAL_PARTICLE_VELOCITY - INITIAL_PARTICLE_VELOCITY,
+		);
+
+		particleSystemGeometry.vertices.push(particle);
+	}
+
+	let particleSystem = new THREE.Points(
+		particleSystemGeometry,
+		particleMaterial
+	);
+
+	scene.add(particleSystem);
+}
+
+// Adjusts velocity of particle towards center of other particles in its visual range
+function updateVelocityForSocialAttraction(particle) {
+	let visualCenter = new THREE.Vector3();
+	let numNeighbors = 0;
+
+	for (let otherParticle of particleSystemGeometry.vertices) {
+		if (particle.distanceTo(otherParticle) < PARTICLE_VISUAL_RANGE) {
+			visualCenter.add(otherParticle);
+			numNeighbors += 1;
+		}
+	}
+
+	if (numNeighbors) {
+		visualCenter.divideScalar(numNeighbors);
+		particle.velocity.add(visualCenter
+			.sub(particle)
+			.multiplyScalar(SOCIAL_ATTRACTION_FACTOR)
+		);
+	}
+}
+
+
+// Adjusts velocity of particle away from other particles that are too close
+function updateVelocityForSocialDistancing(particle) {
+	let personalSpaceNeeded = new THREE.Vector3();
+	for (let otherParticle of particleSystemGeometry.vertices) {
+
+		if (otherParticle !== particle &&
+			particle.distanceTo(otherParticle) < MINIMUM_SOCIAL_DISTANCE) {
+			personalSpaceNeeded
+				.add(particle
+					.clone()
+					.sub(otherParticle)
+				);
+		}
+	}
+
+	particle.velocity
+		.add(
+			personalSpaceNeeded.multiplyScalar(SOCIAL_DISTANCING_FACTOR)
+		);
+}
+
+// Adjusts particle velocity to align with average velocity within visual range
+function updateVelocityForSocialCohesion(particle) {
+	let avgVelocity = new THREE.Vector3();
+	let numNeighbors = 0;
+
+	for (let otherParticle of particleSystemGeometry.vertices) {
+		if (particle.distanceTo(otherParticle) < PARTICLE_VISUAL_RANGE) {
+			avgVelocity.add(otherParticle.velocity);
+			numNeighbors += 1;
+		}
+	}
+
+	if (numNeighbors) {
+		avgVelocity.divideScalar(numNeighbors);
+		const velocityDelta = avgVelocity.sub(particle.velocity);
+		particle.velocity.add(
+			velocityDelta.multiplyScalar(SOCIAL_COHESION_FACTOR)
+		);
+	}
+}
+
+// Prevent particle from going too fast
+function updateVelocityForVelocityLimit(particle) {
+	if (particle.velocity.length() > MAX_PARTICLE_VELOCITY) {
+		particle.velocity = particle.velocity
+			.normalize()
+			.multiplyScalar(MAX_PARTICLE_VELOCITY);
+	}
+}
+
+function updateVelocityForPositionBoundary(particle) {
+	updateVelocityForPositionBoundaryOnDimension(particle, 'x', X_BOUNDARY);
+	updateVelocityForPositionBoundaryOnDimension(particle, 'y', Y_BOUNDARY);
+	updateVelocityForPositionBoundaryOnDimension(particle, 'z', Z_BOUNDARY);
+}
+
+function updateVelocityForPositionBoundaryOnDimension(particle, dimension, absoluteBoundary) {
+	if (particle[dimension] > absoluteBoundary) {
+		particle.velocity[dimension] -= PARTICLE_TURN_ACCELERATION;
+	} else if (particle[dimension] < -1 * absoluteBoundary) {
+		particle.velocity[dimension] += PARTICLE_TURN_ACCELERATION;
+	}
+}
+
+function updatePositionForBoundary(camera) {
+	cameraBoundarySphere.clampPoint(camera.position, camera.position);
+}
+
 function onWindowResize(camera, renderer) {
 
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -41,238 +266,38 @@ function setGradient(geometry, colors, axis, reverse) {
 
 	geometry.computeBoundingBox();
 
-	var bbox = geometry.boundingBox;
-	var size = new THREE.Vector3().subVectors(bbox.max, bbox.min);
+	let bbox = geometry.boundingBox;
+	let size = new THREE.Vector3().subVectors(bbox.max, bbox.min);
 
-	var vertexIndices = ['a', 'b', 'c'];
-	var face, vertex, normalized = new THREE.Vector3(),
+	let vertexIndices = ['a', 'b', 'c'];
+	let face, vertex, normalized = new THREE.Vector3(),
 		normalizedAxis = 0;
 
-	for (var c = 0; c < colors.length - 1; c++) {
+	for (let c = 0; c < colors.length - 1; c++) {
 
-		var colorDiff = colors[c + 1].stop - colors[c].stop;
+		let colorDiff = colors[c + 1].stop - colors[c].stop;
 
-		for (var i = 0; i < geometry.faces.length; i++) {
+		for (let i = 0; i < geometry.faces.length; i++) {
 			face = geometry.faces[i];
-			for (var v = 0; v < 3; v++) {
+			for (let v = 0; v < 3; v++) {
 				vertex = geometry.vertices[face[vertexIndices[v]]];
-				normalizedAxis = normalized.subVectors(vertex, bbox.min).divide(size)[axis];
+				normalizedAxis = normalized
+					.subVectors(vertex, bbox.min)
+					.divide(size)[axis];
+
 				if (reverse) {
 					normalizedAxis = 1 - normalizedAxis;
 				}
+
 				if (normalizedAxis >= colors[c].stop && normalizedAxis <= colors[c + 1].stop) {
-					var localNormalizedAxis = (normalizedAxis - colors[c].stop) / colorDiff;
-					face.vertexColors[v] = colors[c].color.clone().lerp(colors[c + 1].color, localNormalizedAxis);
+					let localNormalizedAxis = (normalizedAxis - colors[c].stop) / colorDiff;
+					face.vertexColors[v] = colors[c].color
+						.clone()
+						.lerp(colors[c + 1].color, localNormalizedAxis);
 				}
 			}
 		}
 	}
-}
-
-
-let scene = new THREE.Scene();
-scene.background = new THREE.Color(0x0f0f0f);
-
-var rev = false;
-var cols = [{
-	stop: 0,
-	color: new THREE.Color(0xf7b000)
-}, {
-	stop: .35,
-	color: new THREE.Color(0xdd0080)
-}, {
-	stop: .5,
-	color: new THREE.Color(0x622b85)
-}, {
-	stop: .75,
-	color: new THREE.Color(0x007dae)
-}, {
-	stop: 1,
-	color: new THREE.Color(0x77c8db)
-}];
-
-
-let camera = new THREE.PerspectiveCamera(
-	CAMERA_FIELD_OF_VIEW,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000,
-);
-camera.position.z = 35;
-
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-var controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
-
-// Skybox
-var geometry = new THREE.SphereGeometry(100, 100, 100);
-var material = new THREE.MeshBasicMaterial({
-	vertexColors: THREE.VertexColors,
-	wireframe: false
-});
-setGradient(geometry, cols, 'y', false);
-material.side = THREE.BackSide;
-var sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
-
-// Sphere to bound the particles
-const cameraBoundarySphere = new THREE.Sphere(new THREE.Vector3(), CAMERA_MAX_POSITION);
-
-
-let particleSystemGeometry = new THREE.Geometry();
-let particalMaterial = new THREE.PointsMaterial({
-	color: 0x000000,
-	size: PARTICLE_SCALE,
-	map: new THREE.TextureLoader().load('../textures/sprites/softcircle.png'),
-	sizeAttenuation: true,
-	alphaTest: 0.5,
-	transparent: true,
-});
-
-
-// now create the individual particles
-for (var p = 0; p < NUM_PARTICLES; p++) {
-	const particle = new THREE.Vector3(
-		Math.cos(Math.random() * Math.PI) * INITIAL_POSITION_RANGE,
-		Math.cos(Math.random() * Math.PI) * INITIAL_POSITION_RANGE,
-		Math.cos(Math.random() * Math.PI) * INITIAL_POSITION_RANGE,
-	);
-
-	// create a velocity vector
-	particle.velocity = new THREE.Vector3(
-		Math.cos(Math.random() * Math.PI) * INITIAL_PARTICLE_VELOCITY,
-		Math.cos(Math.random() * Math.PI) * INITIAL_PARTICLE_VELOCITY,
-		Math.cos(Math.random() * Math.PI) * INITIAL_PARTICLE_VELOCITY,
-	);
-
-	particleSystemGeometry.vertices.push(particle);
-}
-
-// create the particle system
-let particleSystem = new THREE.Points(
-	particleSystemGeometry,
-	particalMaterial
-);
-scene.add(particleSystem);
-
-let animate = () => {
-
-	particleSystemGeometry.vertices.map(vertice => {
-		applySocialAttraction(vertice);
-		applySocialDistancing(vertice);
-		applySocialCohesion(vertice);
-		applyVelocityLimit(vertice);
-
-		applyPositionBoundary(vertice, 'x', X_BOUNDARY);
-		applyPositionBoundary(vertice, 'y', Y_BOUNDARY);
-		applyPositionBoundary(vertice, 'z', Z_BOUNDARY);
-
-		// update the position
-		vertice.add(vertice.velocity);
-	});
-
-	particleSystem.geometry.verticesNeedUpdate = true;
-
-	controls.update();
-	applyCameraPositionBoundary(camera);
-
-	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
-};
-
-// Adjusts velocity of vertice towards center of other vertices in its visual range
-function applySocialAttraction(vertice) {
-	let visualCenter = new THREE.Vector3();
-	let numNeighbors = 0;
-
-	for (let otherVertice of particleSystemGeometry.vertices) {
-		if (vertice.distanceTo(otherVertice) < PARTICLE_VISUAL_RANGE) {
-			visualCenter.add(otherVertice);
-			numNeighbors += 1;
-		}
-	}
-
-	if (numNeighbors) {
-		visualCenter.divideScalar(numNeighbors);
-		vertice.velocity.add(visualCenter
-			.sub(vertice)
-			.multiplyScalar(SOCIAL_ATTRACTION_FACTOR)
-		);
-	}
-}
-
-
-// Adjusts velocity of vertice away from boids that are to close
-function applySocialDistancing(vertice) {
-	let personalSpaceNeeded = new THREE.Vector3();
-	for (let otherVertice of particleSystemGeometry.vertices) {
-
-		if (otherVertice !== vertice &&
-			vertice.distanceTo(otherVertice) < MINIMUM_SOCIAL_DISTANCE) {
-			personalSpaceNeeded
-				.add(vertice
-					.clone()
-					.sub(otherVertice)
-				);
-		}
-	}
-
-	vertice.velocity
-		.add(
-			personalSpaceNeeded.multiplyScalar(SOCIAL_DISTANCING_FACTOR)
-		);
-}
-
-// Adjusts vertice velocity to align with average velocity within visual range
-function applySocialCohesion(vertice) {
-	let avgVelocity = new THREE.Vector3();
-	let numNeighbors = 0;
-
-	for (let otherVertice of particleSystemGeometry.vertices) {
-		if (vertice.distanceTo(otherVertice) < PARTICLE_VISUAL_RANGE) {
-			avgVelocity.add(otherVertice.velocity);
-			numNeighbors += 1;
-		}
-	}
-
-	if (numNeighbors) {
-		avgVelocity.divideScalar(numNeighbors);
-		const velocityDelta = avgVelocity.sub(vertice.velocity);
-		vertice.velocity.add(
-			velocityDelta.multiplyScalar(SOCIAL_COHESION_FACTOR)
-		);
-	}
-}
-
-
-// Speed will naturally vary in flocking behavior, but real animals can't go
-// arbitrarily fast.
-function applyVelocityLimit(vertice) {
-	if (vertice.velocity.length() > MAX_PARTICLE_VELOCITY) {
-		vertice.velocity = vertice.velocity
-			.normalize()
-			.multiplyScalar(MAX_PARTICLE_VELOCITY);
-	}
-}
-
-function applyPositionBoundary(vertice, dimension, absoluteBoundary) {
-	if (vertice[dimension] > absoluteBoundary) {
-		vertice.velocity[dimension] -= PARTICLE_TURN_ACCELERATION;
-	} else if (vertice[dimension] < -1 * absoluteBoundary) {
-		vertice.velocity[dimension] += PARTICLE_TURN_ACCELERATION;
-	}
-}
-
-
-function applyCameraPositionBoundary(camera) {
-	cameraBoundarySphere.clampPoint(camera.position, camera.position);
-}
-
-function init() {
-	window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
 }
 
 init();
